1f1637fe5798cdca9ee3252206d8e47da43e921c
Alain Makoso (Leny) <106040804+lenynzita@users.noreply.github.com>
2022-11-11 18:29:25 -0500

    write to check the functionality

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://lenynzita:Charisma1@cluster0.uutofyf.mongodb.net/myFistDatabase?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
+//mongodb+srv://lenynzita:Charisma1@cluster0.uutofyf.mongodb.net/myFistDatabase?retryWrites=true&w=majority
